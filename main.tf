9d41d51c326ec9f9e1c48ca388ba953b78aac6f0
Uncle_Moris <mjw11@wp.pl>
2024-09-07 15:05:14 +0200

    invalid config ec2

--- main.tf
+++ main.tf
@@ -10,6 +10,14 @@ resource "aws_instance" "main_instance" {
   } 
 }
 
+resource "aws_instance" "test_instance" {
+  ami = "ami-0e04bcbe83a83792e"
+  instance_type = "t2.invalid" 
+  tags = {
+    "Name": "to-fix"
+  } 
+}
+
 variable "aws_region" {
   description = "The AWS region to deploy resources"
   default     = "eu-central-1"
