1cad7540c86558eff98d0d766d92d314a0ab52d0
Uncle_Moris <mjw11@wp.pl>
2024-09-07 17:24:32 +0200

    remove issues

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -13,7 +13,3 @@ variable "tags" {
   description = "Tags to assign to the EC2 instance."
   type        = map(string)
 }
-variable "security_group_ids" {
-  description = "List of Security Group IDs to associate with the EC2 instance."
-  type        = list(string)
-}
