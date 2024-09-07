cb96457a089ca1ee5458a9bf42a4cc7c0a6945cf
Uncle_Moris <mjw11@wp.pl>
2024-09-07 14:45:44 +0200

    initial project structure

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "main_instance" {
+  ami = "ami-0e04bcbe83a83792e"
+  #   instance_type = "t2.micro"
+
+  #   tags = {
+  #     Name = "ExampleEC2Instance"
+  #   }
+}
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  default     = "eu-central-1"
+}
